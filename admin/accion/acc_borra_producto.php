2da15b30cc82733ff2bfed63efe000eb988f2b8f
walterarce <walter.arce@davinci.edu.ar>
2023-11-10 19:52:43 -0300

    Fix
    flor de cagada me habia mandado para el borrado
    
    __DIR__ . "/../../img/productos/" . $producto->getProducto_imagen()

--- admin/accion/acc_borra_producto.php
+++ admin/accion/acc_borra_producto.php
@@ -12,7 +12,14 @@ try {
   $producto = (new Producto())->producto_x_id($id);
   $producto->delete();
   if (!empty($producto->getProducto_imagen())) {
-    (new Imagen())->borrarImagen(__DIR__ . "/../img/productos/" . $producto->getProducto_imagen());
+      echo "borrando imagen";
+      $host = $_SERVER['HTTP_HOST'];
+      $directorio_borrado = dirname(dirname(dirname($_SERVER['PHP_SELF']))) . "/img/productos/" . $producto->getProducto_imagen();
+      echo "<pre>";
+      print_r ("Producto???: ".$producto->getProducto_imagen());
+        print_r($host.$directorio_borrado);
+        echo "</pre>";
+    (new Imagen())->borrarImagen(__DIR__ . "/../../img/productos/" . $producto->getProducto_imagen());
   }
 
    header('Location: ' . dirname(dirname($_SERVER['PHP_SELF'])). '/index.php?sec=productos&ruta=vistas');
