ca06a9eff5f5329d3fb1f6a65ffb7743e5c98c49
walterarce <walter.arce@davinci.edu.ar>
2023-11-15 15:49:31 -0300

    Fix de edicion de subcategoria

--- admin/accion/acc_editar_subcategoria.php
+++ admin/accion/acc_editar_subcategoria.php
@@ -7,6 +7,7 @@ try {
     $subcategoria = (new Subcategoria())->subcategoriaxid($id);
     $subcategoria->setNombre($postData['nombre']);
     $subcategoria->setDescripcion($postData['descripcion']);
+    $subcategoria->setCategoriaId($postData['categoria_id']);
     $subcategoria->actualizar();
     header('Location: ' . dirname($_SERVER['PHP_SELF'], 2) . '/index.php?sec=subcategoria&ruta=vistas');
 } catch (Exception $e) {
--- admin/vistas/adm_editar_subcategoria.php
+++ admin/vistas/adm_editar_subcategoria.php
@@ -1,26 +1,28 @@
 <?php
 $id = $_GET['id'] ?? FALSE;
 $subcategoria = (new Subcategoria())->subcategoriaxid($id);
-$categoria = (new Categoria())->categoriaxid($subcategoria->getCategoriaId());
 $categorias_completas = (new Categoria())->categorias_completas();
-echo "<pre>";
-var_dump($subcategoria);
-echo "</pre>";
 ?>
 
 <div class="">
     <form class="" action="/admin/accion/acc_editar_subcategoria.php?id=<?= $subcategoria->getId(); ?>" method="post">
         <div class="mb-3">
-        <label for="nombre" class="form-label">Nombre</label>
-        <input type="text" class="form-control" id="nombre" name="nombre" value="<?= $subcategoria->getNombre(); ?>">
+            <label for="nombre" class="form-label">Nombre</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" value="<?= $subcategoria->getNombre(); ?>">
         </div>
         <div class="mb-3">
-        <label for="descripcion" class="form-label">Descripción</label>
-        <input type="text" class="form-control" id="descripcion" name="descripcion" value="<?= $subcategoria->getDescripcion(); ?>">
+            <label for="descripcion" class="form-label">Descripción</label>
+            <input type="text" class="form-control" id="descripcion" name="descripcion" value="<?= $subcategoria->getDescripcion(); ?>">
         </div>
         <div class="mb-3">
-        <label for="categoria_id" class="form-label">Categoría</label>
-        <input type="text" class="form-control" id="categoria_id" name="categoria_id" value="<?= $categoria->getNombre() ; ?>">
+            <label for="categoria_id" class="form-label">Categoría</label>
+            <select class="form-control" id="categoria_id" name="categoria_id">
+                <?php foreach ($categorias_completas as $categoria): ?>
+                    <option value="<?= $categoria->getID(); ?>" <?= $categoria->getID() == $subcategoria->getCategoriaId() ? 'selected' : ''; ?>>
+                        <?= $categoria->getNombre(); ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
         </div>
         <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
