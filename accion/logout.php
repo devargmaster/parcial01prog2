7f3d094fe3a018fbb6e194fd930a337a4a21cd94
walterarce <walter.arce@davinci.edu.ar>
2023-11-20 15:34:27 -0300

    fix

--- accion/logout.php
+++ accion/logout.php
@@ -2,10 +2,5 @@
 ob_start();
 (new Autenticacion())->log_out();
 $_SESSION['logout_message'] = "Has cerrado sesión correctamente.";
-
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
-
-    header("Location: " . dirname($_SERVER['PHP_SELF'], 2) . '/index.php?sec=home');
+header("Location: " . dirname($_SERVER['PHP_SELF'], 2) . '/index.php?sec=home');
 
